50bc8ce0d7ef74750a027fac20cb39bd6551defd
Hasan Kagan BAYRAM <hkhaanbayram@outlook.com>
2022-01-02 16:55:43 +0300

    Material ui was added.

--- Project1/ClientApp/src/components/Home.tsx
+++ Project1/ClientApp/src/components/Home.tsx
@@ -1,6 +1,14 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
-
+import {
+    ListItem,
+    ListItemText,
+    ListItemIcon,
+    Icon,
+    Tooltip,
+    IconButton,
+    Button, TextField
+} from "@material-ui/core";
 const Home = () => (
   <div>
     <h1>Hello, world!</h1>
@@ -16,8 +24,13 @@ const Home = () => (
       <li><strong>Development server integration</strong>. In development mode, the development server from <code>create-react-app</code> runs in the background automatically, so your client-side resources are dynamically built on demand and the page refreshes when you modify any file.</li>
       <li><strong>Efficient production builds</strong>. In production mode, development-time features are disabled, and your <code>dotnet publish</code> configuration produces minified, efficiently bundled JavaScript files.</li>
     </ul>
-    <p>The <code>ClientApp</code> subdirectory is a standard React application based on the <code>create-react-app</code> template. If you open a command prompt in that directory, you can run <code>npm</code> commands such as <code>npm test</code> or <code>npm install</code>.</p>
-  </div>
+        <p>The <code>ClientApp</code> subdirectory is a standard React application based on the <code>create-react-app</code> template. If you open a command prompt in that directory, you can run <code>npm</code> commands such as <code>npm test</code> or <code>npm install</code>.</p>
+        <Button variant="outlined" href="#outlined-buttons">
+            Click ME
+        </Button>
+        <TextField id="standard-basic" label="Standard" variant="standard" />
+
+    </div>
 );
 
 export default connect()(Home);
